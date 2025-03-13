45e57dcab8fad8ecd8eee1fe85e26110b79a6007
Julia Gromna <julia.gromna@student.pk.edu.pl>
2025-03-13 10:30:01 +0100

    hotjar added

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,27 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+// describe('Swiper Gallery Test', function () {
+//   it('Checks if buttons next and prev work properly', function () {
+//     cy.visit('http://localhost:3000');
+//     cy.get('.swiper-button-next').click();
+//     cy.get('.swiper-slide-active').parent().children().eq(1).should('have.class', 'swiper-slide-active');
+//     cy.wait(2000);
+//     cy.get('.swiper-button-next').click({ force: true });
+//     cy.get('.swiper-slide-active').parent().children().eq(2).should('have.class', 'swiper-slide-active');
+//     cy.wait(2000);
+//     cy.get('.swiper-button-next').click({ force: true });
+//     cy.get('.swiper-slide-active').parent().children().eq(0).should('have.class', 'swiper-slide-active');
+//     cy.wait(2000);
+//     cy.get('.swiper-button-prev').click({ force: true });
+//     cy.get('.swiper-slide-active').parent().children().eq(2).should('have.class', 'swiper-slide-active');
+//     cy.wait(2000);
+//     cy.get('.swiper-button-prev').click({ force: true });
+//     cy.get('.swiper-slide-active').parent().children().eq(1).should('have.class', 'swiper-slide-active');
+//     cy.wait(2000);
+//     cy.get('.swiper-button-prev').click({ force: true });
+//     cy.get('.swiper-slide-active').parent().children().eq(0).should('have.class', 'swiper-slide-active');
+//     cy.wait(2000);
+//   });
+// });
